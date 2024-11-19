1f3b92e1d372f20fa20d2dc954829ed9aa27ce8d
Kyle Murison <kylemurison@proton.me>
2024-11-18 23:57:37 -0800

    adding a comment

--- CMtoInches.java
+++ CMtoInches.java
@@ -8,11 +8,18 @@ package com.mycompany.info4190;
  *
  * @author bigch
  */
+import java.util.Scanner;
 public class CMtoInches {
 
     public static void main(String[] args) {
-        double cm = 5, inch = 0;
-        inch = cm * 0.3937008;
+       
+        /**
+         * adding a comment here, hello world
+         */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the amount of centimeters");
+        Double cm = input.nextDouble();
+        Double inch = cm * 0.3937008;
         System.out.println(cm + " centimeters is equal to " + inch + " inches!");
     }
 }
