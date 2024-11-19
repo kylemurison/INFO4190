3e07f037dfd1ffad9ba087a7d9844c35e8eb6f0e
Kyle Murison <kylemurison@proton.me>
2024-11-18 23:04:12 -0800

    adding code

---
+++ CMtoInches.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.info4190;
+
+/**
+ *
+ * @author bigch
+ */
+public class CMtoInches {
+
+    public static void main(String[] args) {
+        double cm = 5, inch = 0;
+        inch = cm * 0.3937008;
+        System.out.println(cm + " centimeters is equal to " + inch + " inches!");
+    }
+}
