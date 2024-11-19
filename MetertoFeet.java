40f7ef4746f35ba9db42560b70b9310382fbd665
Kyle Murison <kylemurison@proton.me>
2024-11-18 23:13:53 -0800

    adding code

---
+++ MetertoFeet.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.info4190;
+
+/**
+ *
+ * @author bigch
+ */
+public class MetertoFeet {
+
+    public static void main(String[] args) {
+        double meter = 5, feet = 0;
+        feet = meter * 3.28084;
+        System.out.println(meter + " meters is equal to " + feet + " feet!");
+    }
+}
